fbbe42f5b728a6e965d56ac5bdcdf6d7861600df
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-24 10:21:20 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,17 @@
+# terraform/providers.tf
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.87"
+    }
+  }
+}
+
+# Configure the Snowflake Provider
+provider "snowflake" {
+  account  = var.snowflake_account
+  username = var.snowflake_username
+  password = var.snowflake_password
+  role     = "ACCOUNTADMIN"  # Need ACCOUNTADMIN to change ownership
+}
