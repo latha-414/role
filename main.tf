aa09339fec4615fe929d4a9902d9b1d7bdf1c5ab
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-24 10:28:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-# Import the existing HCPUSER role
-resource "snowflake_role" "hcpuser" {
+# Import the existing HCPUSER role (using new resource type)
+resource "snowflake_account_role" "hcpuser" {
   name    = "HCPUSER"
   comment = "HCP User role - ownership transferred to SYSADMIN"
 }
 
 # Grant ownership of HCPUSER role to SYSADMIN
-resource "snowflake_role_ownership_grant" "hcpuser_ownership" {
-  role_name   = snowflake_role.hcpuser.name
-  to_role     = "SYSADMIN"
+resource "snowflake_grant_ownership" "hcpuser_ownership" {
+  account_role_name = snowflake_account_role.hcpuser.name
+  to_role_name      = "SYSADMIN"
   
   # This ensures the grant happens after the role is managed by Terraform
-  depends_on = [snowflake_role.hcpuser]
+  depends_on = [snowflake_account_role.hcpuser]
 }
