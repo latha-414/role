4e7188eec824f064fdb2c9f93e553e53b3a0d1af
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-24 10:21:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# terraform/variables.tf
+variable "snowflake_account" {
+  description = "Snowflake account identifier"
+  type        = string
+}
+
+variable "snowflake_username" {
+  description = "Snowflake username"
+  type        = string
+}
+
+variable "snowflake_password" {
+  description = "Snowflake password"
+  type        = string
+  sensitive   = true
+}
