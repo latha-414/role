5a375f815af33bc92e7a9e0c8ebfb86799ead425
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-24 10:35:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,9 @@ resource "snowflake_account_role" "hcpuser" {
 }
 
 # Grant ownership of HCPUSER role to SYSADMIN
-resource "snowflake_grant_ownership" "hcpuser_ownership" {
-  on {
-    object_type = "ROLE"
-    object_name = snowflake_account_role.hcpuser.name
-  }
-  
-  to {
-    account_role_name = "SYSADMIN"
-  }
+resource "snowflake_role_ownership_grant" "hcpuser_ownership" {
+  role_name = snowflake_account_role.hcpuser.name
+  to_role   = "SYSADMIN"
   
   # This ensures the grant happens after the role is managed by Terraform
   depends_on = [snowflake_account_role.hcpuser]
